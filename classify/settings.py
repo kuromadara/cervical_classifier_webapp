c8a0212ea790903ef79a84a867a86f6c54495681
kuromadara <dowarah.dayananda35@gmail.com>
2021-04-06 01:56:21 +0530

    Proc

--- classify/settings.py
+++ classify/settings.py
@@ -122,4 +122,4 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
     )
-STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles') 
+STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
